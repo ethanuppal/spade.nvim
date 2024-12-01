62f36eba91706e5b5bed9aa66e2e8d1dad1b352d
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2024-12-01 18:01:19 -0500

    Try to add LSP

--- README.md
+++ README.md
@@ -2,14 +2,28 @@
 
 ## Install
 
+First, you'll want to grab the Spade LSP (you might need to [install
+rust](https://www.rust-lang.org/tools/install)):
+
+```sh
+cargo install --locked --git https://gitlab.com/spade-lang/spade-language-server
+```
+
 Install the plugin with your preferred package manager.
 Here's how it would look like if you're using [lazy.nvim](https://github.com/folke/lazy.nvim):
 
 ```lua
 {
-    "ethanuppal/spade.nvim", 
+    "ethanuppal/spade.nvim",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        { url = "https://gitlab.com/spade-lang/spade-vim" },
+    },
     config = function()
-        require("spade").setup()
+        require("spade").setup({
+            -- leave as {} for default options
+            lsp_command = "spade-language-server"
+        })
     end
 }
 ```
--- lua/spade/init.lua
+++ lua/spade/init.lua
@@ -1,20 +1,48 @@
-return {
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-		{ url = "https://gitlab.com/spade-lang/spade-vim" },
-	},
-	setup = function()
-		-- see https://github.com/nvim-treesitter/nvim-treesitter
-		require("nvim-treesitter.install").prefer_git = true
-		require("nvim-treesitter.parsers").get_parser_configs()["spade"] = {
-			install_info = {
-				url = "https://gitlab.com/spade-lang/tree-sitter-spade/",
-				files = { "src/parser.c" },
-				branch = "main",
-				generate_requires_npm = false,
-				requires_generate_from_grammar = false,
-			},
-			filetype = "spade",
-		}
-	end,
+local M = {}
+
+M.config = {
+	lsp_command = "spade-language-server",
 }
+
+local function setup_treesitter()
+	-- see https://github.com/nvim-treesitter/nvim-treesitter
+	require("nvim-treesitter.install").prefer_git = true
+	require("nvim-treesitter.parsers").get_parser_configs()["spade"] = {
+		install_info = {
+			url = "https://gitlab.com/spade-lang/tree-sitter-spade/",
+			files = { "src/parser.c" },
+			branch = "main",
+			generate_requires_npm = false,
+			requires_generate_from_grammar = false,
+		},
+		filetype = "spade",
+	}
+end
+
+local function setup_lsp()
+	vim.lsp.start({
+		cmd = { M.config.lsp_command },
+		root_dir = vim.fn.getcwd(),
+	})
+
+	-- vim.api.nvim_create_autocmd("FileType", {
+	-- 	pattern = "spade",
+	-- 	callback = function()
+	-- 		-- Your custom LSP setup for Spade files
+	-- 		vim.lsp.start({
+	-- 			name = "spade-lsp",
+	-- 			cmd = { M.config.lsp_command },
+	-- 			root_dir = vim.fs.dirname(vim.fs.find({ "swim.toml" }, { upward = true })[1]),
+	-- 		})
+	-- 	end,
+	-- })
+end
+
+function M.setup(opts)
+	M.config = vim.tbl_deep_extend("force", M.config, opts or {})
+
+	setup_treesitter()
+	setup_lsp()
+end
+
+return M
