ed61fc4035a32df3496ca9bd296e62ccf9088e79
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2024-12-01 14:51:14 -0500

    Try fix

---
+++ lua/spade/init.lua
@@ -0,0 +1,16 @@
+return {
+	setup = function()
+		-- https://github.com/nvim-treesitter/nvim-treesitter
+		require("nvim-treesitter.install").prefer_git = true
+		require("nvim-treesitter.parsers").get_parser_configs()["spade"] = {
+			install_info = {
+				url = "https://gitlab.com/spade-lang/tree-sitter-spade/",
+				files = { "src/parser.c" },
+				branch = "main",
+				generate_requires_npm = false,
+				requires_generate_from_grammar = false,
+			},
+			filetype = "spade",
+		}
+	end,
+}
