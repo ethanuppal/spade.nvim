f39a822bb9cf21a6f495f86818611a6cb1c4ac95
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2024-12-04 03:31:24 -0500

    Add `openSwim` subcommand (#5)
    * Add go to swim.toml
    
    * Fix completion

--- lua/spade/init.lua
+++ lua/spade/init.lua
@@ -1,8 +1,70 @@
+-- Based off of https://github.com/NMAC427/guess-indent.nvim
+
 local M = {}
 
 M.config = {
 	lsp_command = "spade-language-server",
 }
+
+--- Determines the closest parent directory containing `swim.toml`.
+function M.swim_root_dir()
+	local current_buffer_path = vim.fn.expand("%:p")
+	if current_buffer_path == nil then
+		current_buffer_path = vim.uv.cwd()
+	end
+	return vim.fs.dirname(vim.fs.find({ "swim.toml" }, {
+		path = current_buffer_path,
+		type = "file",
+		upward = true,
+	})[1])
+end
+
+--- Run when you need to make sure that you're working in a valid Spade project.
+function M.check_health()
+	if M.swim_root_dir() == nil then
+		vim.notify(
+			"Spade: cannot find `swim.toml` in parent directory. Are you sure you're currently in a Spade project?",
+			vim.log.levels.WARN
+		)
+		return
+	end
+end
+
+local function install_command()
+	local subcommands = {
+		openSwim = function(_)
+			M.check_health()
+
+			vim.cmd.edit(vim.fs.joinpath(M.swim_root_dir(), "swim.toml"))
+		end,
+	}
+
+	vim.api.nvim_create_user_command("Spade", function(args)
+		local subcommand = args.fargs[1]
+
+		if not subcommand then
+			vim.notify("`:Spade`: no subcommand provided", vim.log.levels.ERROR)
+			return
+		end
+
+		if subcommands[subcommand] ~= nil then
+			subcommands[subcommand](vim.list_slice(args.fargs, 2))
+		else
+			vim.notify("`:Spade`: invalid subcommand `" .. subcommand .. "`", vim.log.levels.ERROR)
+			return
+		end
+	end, {
+		nargs = "*",
+		complete = function(_, line)
+			if string.match(vim.trim(line), "^Spade") then
+				return vim.tbl_keys(subcommands)
+			end
+
+			return {}
+		end,
+		desc = "Spade language support for Neovim",
+	})
+end
 
 local function setup_treesitter()
 	-- see https://github.com/nvim-treesitter/nvim-treesitter
@@ -24,11 +86,17 @@ local function setup_lsp()
 	vim.api.nvim_create_autocmd("FileType", {
 		pattern = "spade",
 		callback = function()
-			vim.lsp.start({
-				name = "spade-lsp",
-				cmd = { M.config.lsp_command },
-				root_dir = vim.fs.dirname(vim.fs.find({ "swim.toml" }, { upward = true })[1]),
-			})
+			install_command()
+
+			if M.swim_root_dir() ~= nil then
+				vim.lsp.start({
+					name = "spade-lsp",
+					cmd = { M.config.lsp_command },
+					root_dir = M.swim_root_dir(),
+				})
+			else
+				vim.notify("No `swim.toml` configuration found", vim.log.levels.WARN)
+			end
 		end,
 	})
 end
